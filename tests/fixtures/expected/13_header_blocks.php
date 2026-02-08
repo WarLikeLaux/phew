c6e4573c0b6ebfbce4e20fda0a91e3f95cc8babd
WarLikeLaux <warlikelaux080399@mail.ru>
2026-02-08 17:42:03 +0600

    #2 - added fixture 13: header block separation

---
+++ tests/fixtures/expected/13_header_blocks.php
@@ -0,0 +1,13 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+$this->title = 'Create';
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+
+<div class="author-form">
+    <?= $form->field($model, 'fio') ?>
+</div>
