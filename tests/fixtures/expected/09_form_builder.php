ff9d7fa996746448e2a0323f24da3f13846be2a3
WarLikeLaux <warlikelaux080399@mail.ru>
2026-02-08 18:13:54 +0600

    #2 - updated fixture 09: inline <?php/?> for template blocks

--- tests/fixtures/expected/09_form_builder.php
+++ tests/fixtures/expected/09_form_builder.php
@@ -1,12 +1,9 @@
 <div class="author-form">
-    <?php
-
-    $form = ActiveForm::begin([
+    <?php $form = ActiveForm::begin([
         'fieldClass' => ActiveField::class,
         'options' => ['enctype' => 'multipart/form-data'],
         'enableAjaxValidation' => true,
-    ]);
-    ?>
+    ]); ?>
     <?= $form->errorSummary($model) ?>
     <?= $form->field($model, 'fio')->textInput(['maxlength' => true]) ?>
     <?= $form->field($model, 'year')->textInput(['type' => 'number']) ?>
