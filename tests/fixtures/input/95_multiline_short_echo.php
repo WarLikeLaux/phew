e7582b4d41ab1b727f373a6732e4c156852d3bad
WarLikeLaux <warlikelaux080399@mail.ru>
2026-03-09 03:18:33 +0600

    #11 - added fixture for multiline short echo tag formatting

---
+++ tests/fixtures/input/95_multiline_short_echo.php
@@ -0,0 +1,31 @@
+<div>
+    <span><?=
+        23
+    ?></span>
+    <p><?=
+        $variable
+    ?></p>
+    <td><?=
+        Html::encode($model->name)
+    ?></td>
+</div>
+
+<?=
+    $standalone
+?>
+
+
+
+<?=
+    Yii::t('app', 'Hello')
+?>
+
+<?=
+    $a
+?>
+<?=
+    $b
+?>
+<?=
+    $c
+?>
